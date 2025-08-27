949283af026a2eeadfa961ab7d3cfb35444e0cfb
wyatt4543 <w4543w@gmail.com>
2025-08-27 12:19:15 -0500

    Edited js to fix the script

--- Week2/main.js
+++ Week2/main.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     function createParagraph() {
         let para = document.createElement("p");
-        para.textContent = "You clicked the button";
+        para.textContent = "you clicked the button";
         document.body.appendChild(para);
     }
 
     const buttons = document.querySelectorAll("button");
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", createParagraph())
+        buttons[i].addEventListener("click", createParagraph);
     }
 });
