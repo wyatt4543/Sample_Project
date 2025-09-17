36d36f9bd48f2ef929c5de67d817aced7508919b
wyatt4543 <w4543w@gmail.com>
2025-09-17 12:17:15 -0500

    Make seperate folders for day 1 and 2 for week 5

---
+++ Week 5/5-2/js/arrays.js
@@ -0,0 +1,22 @@
+//using array literal
+let fruits = ["apple", "banana", "orange"]
+
+console.log(fruits);
+//Array constructor
+let numbers = new Array(1, 2, 3, 45, 6);
+
+let mixData = ["string", 32, null, ["test 1", "test 2"]];
+
+console.log(mixData[3][0])
+
+console.log(fruits[2]);
+
+fruits[3] = "berry";
+console.log(fruits);
+
+console.log(fruits.length);
+
+fruits.push("dates");
+
+console.log(fruits);
+
